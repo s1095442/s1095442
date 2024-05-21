499a760c8a4e3646c81170c281539c484e5ce99d
Joii <s1095442@gmail.com>
2024-05-21 19:55:56 +0800

    v2_林佳儀

--- app/src/main/java/tw/edu/pu/s1095442/s1095442/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1095442/s1095442/MainActivity.kt
@@ -1,15 +1,29 @@
 package tw.edu.pu.s1095442.s1095442
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.rounded.AccountBox
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import tw.edu.pu.s1095442.s1095442.ui.theme.S1095442Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +32,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             S1095442Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Main()
                 }
             }
         }
@@ -27,17 +44,91 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FirstScreen(navController: NavHostController, modifier: Modifier = Modifier) {
+    val context = LocalContext.current  //取得App的運行環境
+    Column(modifier = Modifier.fillMaxSize()) {
+        Column {
+
+            Text(
+                text = "簡介",
+                color = Color.Blue,
+                modifier = modifier
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    S1095442Theme {
-        Greeting("Android")
+fun SecondScreen(navController: NavHostController) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = "主要機構",
+            color = Color.Red
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Main() {
+    val navController = rememberNavController()
+    val context = LocalContext.current
+    var showMenu by remember { mutableStateOf(false) }
+
+    Column {
+        androidx.compose.material3.TopAppBar(
+            title = { Image(
+                painter = painterResource(id = R.drawable.maria),
+                contentDescription = null
+            ) },
+            navigationIcon = {
+                IconButton(onClick = {
+                    Toast.makeText(context, "", Toast.LENGTH_SHORT).show()
+                }) {
+                    Icon(Icons.Default.Menu, contentDescription = "Navigation icon")
+                }
+            },
+            actions = {
+                IconButton(
+                    onClick = {
+                        Toast.makeText(context, "一", Toast.LENGTH_SHORT).show()
+                    }
+                ) {
+                    Icon(Icons.Rounded.AccountBox, contentDescription = "Author")
+                }
+                IconButton(
+                    onClick = { showMenu = true }
+                ) {
+                    Icon(Icons.Default.MoreVert, contentDescription = "More")
+                }
+                DropdownMenu(
+                    expanded = showMenu, onDismissRequest = { showMenu = false }
+                ) {
+                    DropdownMenuItem(
+                        text = { Text("簡介") },
+                        onClick = {
+                            showMenu = false
+                            navController.navigate("JumpFirst")
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("主要機構") },
+                        onClick = {
+                            showMenu = false
+                            navController.navigate("JumpSecond")
+                        }
+                    )
+                }
+            }
+        )
+
+        NavHost(navController = navController, startDestination = "JumpFirst") {
+            composable("JumpFirst") {
+                FirstScreen(navController = navController)
+            }
+            composable("JumpSecond") {
+                SecondScreen(navController = navController)
+            }
+        }
     }
 }
